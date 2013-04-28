820fb8037d34feebf51ad72d1156f2de6e1fe5c7
Daniel Labas <daniel@dlabas.de>
2013-04-28 21:30:06 +0200

    Fix bug in edit use case form

--- src/DlcUseCase/Form/EditUseCase.php
+++ src/DlcUseCase/Form/EditUseCase.php
@@ -4,13 +4,31 @@ namespace DlcUseCase\Form;
 use DlcUseCase\Form\BasePriority;
 use Zend\Form\Element;
 use Zend\Form\Form as ZendForm;
+use Zend\Form\FormInterface;
 
 class EditUseCase extends BaseUseCase
 {
     public function init()
     {
         parent::init();
-        
+
         $this->setLabel('Edit use case');
+    }
+
+    public function bind($useCase, $flags = FormInterface::VALUES_NORMALIZED)
+    {
+        parent::bind($useCase, $flags);
+
+        if ($useCase->getType() != null) {
+            $this->byName['type']->setValue($useCase->getType()->getId());
+        }
+
+        if ($useCase->getCategory() != null) {
+            $this->byName['category']->setValue($useCase->getCategory()->getId());
+        }
+
+        if ($useCase->getPriority() != null) {
+            $this->byName['priority']->setValue($useCase->getPriority()->getId());
+        }
     }
 }
